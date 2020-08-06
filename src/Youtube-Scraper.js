2d95cdce1674e8512c034839b280400ac00f0b79
Luca <hohmann.luca@tutanota.com>
2020-08-06 22:53:48 +0200

    HTML extraction, Script extraction Added: 1) HTML extraction - Cheerio now parses the data from the webpage request to html 2) Script extraction - Using cheerios searching method, the correct script tag (number 26) is extracted because there are the video informations in an almost correct JSON format. With some further changes it can be parsed as json and processed further

--- src/Youtube-Scraper.js
+++ src/Youtube-Scraper.js
@@ -1,5 +1,7 @@
-const util = require("util")
+const fs = require("fs")
 const requester = require("./TrendingRequester")
+const cheerio = require('cheerio');
+
 
 class YoutubeScraper {
     static scrape_trending_page() {
@@ -9,16 +11,36 @@ class YoutubeScraper {
     }
 
     static data_receive_ok(request_data){
-        console.log(">Success requesting trending page")
-        this.parse_html(request_data.data)
+        console.log(">Success requesting trending page");
+        this.parse_html(request_data.data);
     }
 
     static data_receive_failed(error){
-        console.log("ERROR")
+        console.log("ERROR", error);
     }
 
     static parse_html(html_data){
-        console.log(html_data)
+        /*fs.writeFile("./Received_data.html", html_data, (err) =>{
+            if(err){
+                throw err;
+            }
+            console.log("Wrote Data Successfully")
+
+        });*/
+        let html_parsed = cheerio.load(html_data)
+        let extracted_script_tag = html_parsed('script').get()[26].children[0].data
+
+        // debugging purposes
+        fs.writeFile("./test.json", extracted_script_tag, (err) =>{
+            if(err){
+                throw err;
+            }
+            console.log("Wrote Data Successfully")
+
+        });
+        //TODO remove the first part of string up to the first curly open bracket and remove the last parts of the string after the closing curly bracket
+
+        //console.log($)
     }
 }
 module.exports = YoutubeScraper
