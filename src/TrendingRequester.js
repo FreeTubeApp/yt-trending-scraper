91e6ecb5130d79c6fb0d9756bf2ad3dcec52e46d
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-08-29 20:44:50 -0400

    Add linting to project (#20)
    * lint project, add lint workflow, update version
    
    * Make parameter names lowercase
    
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>
    
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/TrendingRequester.js
+++ src/TrendingRequester.js
@@ -1,41 +1,33 @@
-const axios = require("axios")
-const trendingPageBase = "https://www.youtube.com/feed/trending"
+const axios = require('axios')
+const trendingPageBase = 'https://www.youtube.com/feed/trending'
 const pageAdditions = {
-    'music': '4gINGgt5dG1hX2NoYXJ0cw%3D%3D',
-    'gaming': '4gIcGhpnYW1pbmdfY29ycHVzX21vc3RfcG9wdWxhcg%3D%3D',
-    'movies': '4gIKGgh0cmFpbGVycw%3D%3D'
+  music: '4gINGgt5dG1hX2NoYXJ0cw%3D%3D',
+  gaming: '4gIcGhpnYW1pbmdfY29ycHVzX21vc3RfcG9wdWxhcg%3D%3D',
+  movies: '4gIKGgh0cmFpbGVycw%3D%3D'
 }
 
 class TrendingRequester {
-    static async requestTrendingPage(geoLocation = null, page) {
-        const config = {
-            headers: {
-                'x-youtube-client-name': '1',
-                'x-youtube-client-version': '2.20180222',
-                'accept-language': 'en-US,en;q=0.5'
-            }
-        }
+  static async requestTrendingPage(geoLocation = null, page) {
+    try {
+      const params = {}
+      if (geoLocation !== null) {
+        params.persist_gl = 1
+        params.gl = geoLocation
+      }
+      if (page !== 'default') {
         try {
-            const params = {}
-            if (geoLocation !== null) {
-                params['persist_gl'] = 1
-                params['gl'] = geoLocation
-            }
-            if (page !== 'default') {
-                try {
-                    params['bp'] = pageAdditions[page]
-                } catch (error) {
-                    console.error("Fallback to default trending page because no valid page name was provided:", page)
-                }
-            }
-            return await axios.get(trendingPageBase, {params})
-
-        } catch (e) {
-            return {
-                error: true,
-                message: e
-            }
+          params.bp = pageAdditions[page]
+        } catch (error) {
+          console.error('Fallback to default trending page because no valid page name was provided:', page)
         }
+      }
+      return await axios.get(trendingPageBase, { params })
+    } catch (e) {
+      return {
+        error: true,
+        message: e
+      }
     }
+  }
 }
 module.exports = TrendingRequester
--- test/scraper.test.js
+++ test/scraper.test.js
@@ -1,65 +1,64 @@
 const scraper = require('../index')
 describe('Scraper Test', () => {
-    test('Scrape no arguments', () => {
+  test('Scrape no arguments', () => {
+    return scraper.scrapeTrendingPage().then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-        return scraper.scrape_trending_page().then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape default page', () => {
+    const parameters = { page: 'default' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape default page', () => {
-        const parameters = {page: 'default'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape music page', () => {
+    const parameters = { page: 'music' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape music page', () => {
-        const parameters = {page: 'music'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape movies page', () => {
+    const parameters = { page: 'movies' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape movies page', () => {
-        const parameters = {page: 'movies'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape gaming page', () => {
+    const parameters = { page: 'gaming' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape gaming page', () => {
-        const parameters = {page: 'gaming'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape illegal page', () => {
+    const parameters = { page: 'ThisIsNotAnOption' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape illegal page', () => {
-        const parameters = {page: 'ThisIsNotAnOption'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape parseCreatorOnRise', () => {
+    const parameters = { parseCreatorOnRise: 'true' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape parseCreatorOnRise', () => {
-        const parameters = {parseCreatorOnRise: 'true'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
+  test('Scrape Japan Geolocation', () => {
+    const parameters = { geoLocation: 'JP' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
 
-    test('Scrape Japan Geolocation', () => {
-        const parameters = {geoLocation: 'JP'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
-
-    test('Scrape Japan Geolocation With Creator On Rise On Music Page', () => {
-        const parameters = {geoLocation: 'JP', parseCreatorOnRise: 'true', page: 'music'};
-        return scraper.scrape_trending_page(parameters).then((data) => {
-            expect(data).not.toHaveLength(0);
-        });
-    });
-});
+  test('Scrape Japan Geolocation With Creator On Rise On Music Page', () => {
+    const parameters = { geoLocation: 'JP', parseCreatorOnRise: 'true', page: 'music' }
+    return scraper.scrapeTrendingPage(parameters).then((data) => {
+      expect(data).not.toHaveLength(0)
+    })
+  })
+})
