c7ac3a7294cfeef73d376aed04fa70fc2e991825
onion chunky <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-09-06 22:05:08 -0400

    add type definition

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+declare module "yt-trending-scraper" {
+  interface Thumbnail {
+    quality: string
+    url: string
+    width: number
+    height: number
+  }
+  interface Video {
+    videoId: string,
+    title: string,
+    type: "video",
+    author: string,
+    authorId: string,
+    authorUrl: string,
+    videoThumbnails: Thumbnail[],
+    description: string,
+    viewCount: number,
+    published: EpochTimeStamp,
+    publishedText: string,
+    lengthSeconds: number,
+    timeText: string,
+    liveNow: false,
+    paid: false,
+    premium: false,
+    isUpcoming: false,
+    isCreatorOnRise: boolean,
+    isVerified: boolean,
+    isVerifiedAuthor: boolean,
+    isShort: boolean
+  }
+  interface TrendingPayload {
+    geoLocation: string,
+    parseCreatorOnRise: boolean,
+    page: "default" | "music" | "gaming" | "movies"
+  }
+  class TrendingScraper {
+    static scrapeTrendingPage(payload: TrendingPayload) : Promise<Video[]>
+  }
+}
